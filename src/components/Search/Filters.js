9af44cc95984200ddcbfc43f7fa02dcc76871457
unknown <s.g.bibas@gmail.com>
2020-02-19 00:00:34 +0200

    add filters

--- src/components/Search/Filters.js
+++ src/components/Search/Filters.js
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Rating, Dropdown, Button } from 'semantic-ui-react';
+import styles from './Filters.module.css';
 
-export default function Filters() {
+export default function Filters({ options, setFilters, setFiltersAreOpen }) {
+  const initialState = {
+    from: null,
+    to: null,
+    rating: 0,
+    publisher: null
+  };
+  const [values, setValues] = useState(initialState);
+
+  const uniqueOptions = options.filter((item, index) => {
+    return options.indexOf(item) === index;
+  });
+
+  const {
+    filtersContainer,
+    filterLabel,
+    filter,
+    filterInput,
+    filterButtons,
+  } = styles;
+
   return (
-
+    <div className={filtersContainer}>
+      <p className={filterLabel}>Year</p>
+      <div className={filter}>
+        <p>from</p>
+        <input
+          className={filterInput}
+          value={values.from}
+          onChange={e => setValues({
+            ...values,
+            from: e.target.value
+          })}
+        />
+        <p>to</p>
+        <input
+          className={filterInput}
+          value={values.to}
+          onChange={e => setValues({
+            ...values,
+            to: e.target.value
+          })}
+        />
+      </div>
+      <p className={filterLabel}>Rating</p>
+      <div className={filter}>
+        <Rating
+          icon="star"
+          rating={values.rating}
+          maxRating={5}
+          onRate={(e, data) => setValues({ ...values, rating: data.rating })}
+        />
+      </div>
+      <p className={filterLabel}>Publisher</p>
+      <div className={filter}>
+        <Dropdown placeholder={values.publisher || 'Select Publisher'}>
+          <Dropdown.Menu>
+            {uniqueOptions.map(option => (
+              <Dropdown.Item
+                key={option}
+                text={option}
+                onClick={(e, data) => setValues({ ...values, publisher: data.text })}
+              />
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <div className={filterButtons}>
+        <Button color="red" onClick={() => setValues(initialState)}>Clear All</Button>
+        <Button onClick={() => {
+          setFilters(values);
+          setFiltersAreOpen(false);
+        }}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
   );
 }
+
+Filters.propTypes = {
+  options: PropTypes.array.isRequired,
+  setFilters: PropTypes.func.isRequired,
+  setFiltersAreOpen: PropTypes.func.isRequired,
+};
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Icon, Loader, Segment, Dimmer
 } from 'semantic-ui-react';
 import Context from '../../context';
+import Filters from './Filters';
 import Book from './Book';
 import styles from './Search.module.css';
 
 export default function Search() {
   const { state, actions: { searchBooks } } = useContext(Context);
+  const [filtersAreOpen, setFiltersAreOpen] = useState(false);
+  const [filters, setFilters] = useState(null);
 
   const handleSearchChange = (query) => {
     const filteredBooks = state.books.filter(
@@ -22,15 +25,42 @@ export default function Search() {
     searchBooks(filteredBooks);
   };
 
+  useEffect(
+    () => {
+      if (state.books && filters) {
+        let filteredBooks;
+        if (filters.rating) {
+          filteredBooks = state.books.filter(book => book.rating === filters.rating);
+        }
+        if (filters.from) {
+          filteredBooks = state.books.filter(book => book.year >= filters.from);
+        }
+        if (filters.to) {
+          filteredBooks = state.books.filter(book => book.year <= filters.to);
+        }
+        if (filters.publisher) {
+          filteredBooks = state.books.filter(book => book.publisher === filters.publisher);
+        }
+        searchBooks(filteredBooks);
+      } else {
+        searchBooks(state.books);
+      }
+    },
+    [filters]
+  );
+
   const {
     searchbarContainer,
     searchbar,
-    filters,
+    filtersContainer,
+    filtersBtn,
     filterIcon,
     bookList,
     btnAddProduct,
     loader,
     errorMessage,
+    clearFilters,
+    clearFiltersBtn
   } = styles;
 
   const showAllBooks = !state.filteredBooks && state.books;
@@ -46,9 +76,21 @@ export default function Search() {
         />
         <Icon name="search" size="large" />
       </div>
-      <div className={filters}>
-        <p>Filters</p>
-        <Icon name="filter" size="small" className={filterIcon} />
+      <div className={filtersContainer}>
+        <div className={filtersBtn} onClick={() => setFiltersAreOpen(prev => !prev)}>
+          <p>Filters</p>
+          <Icon name="filter" size="small" className={filterIcon} />
+        </div>
+        {filters && (
+          <div className={clearFiltersBtn} onClick={() => setFilters(null)}>
+            <Icon
+              color="red"
+              name="times"
+              className={clearFilters}
+            />
+            <p>clear filters</p>
+          </div>
+        )}
       </div>
       {state.loading && (
         <Segment className={loader}>
@@ -66,6 +108,14 @@ export default function Search() {
         </Segment>
       )}
       <div className={bookList}>
+        {filtersAreOpen && state.books && (
+          <Filters
+            options={state.books.map(b => { return b.publisher })}
+            filters={filters}
+            setFilters={setFilters}
+            setFiltersAreOpen={setFiltersAreOpen}
+          />
+        )}
         {showAllBooks && state.books.map((b, i) => (
           <Book
             index={i}
